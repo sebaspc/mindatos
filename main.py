2f561b9db7140bc747dcf9120c4a285b13f290de
Dizzymo <s.painen@outlook.cl>
2022-04-19 02:33:56 -0400

    spc 3 - pronosticando subida del pib 6% v1

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
-from operator import index
-from os import name
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+
 
 df = pd.read_csv(r'weatherAUS.csv')
 #cantidad de datos filas x columns
@@ -17,8 +17,10 @@ df['Month'] = df['Date'].dt.month
 df['Day'] = df['Date'].dt.day
 
 #canidad de valores nulos x columnas
-#print(df.isnull().sum())
-#print(df.info())
+print(df.isnull().sum())
+print(df.info())
+#transformando dato nulos de raintoday a Null
+df['RainToday'].replace(['NA'], [0], inplace=True)
 
 #borrando filas en null
 #df = df.dropna(subset=['MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine' ])
@@ -28,16 +30,16 @@ df['Day'] = df['Date'].dt.day
 #locationYes = df.loc[df["RainToday"] == 'Yes', ["Location", "Year"]]
 #locationNo = df.loc[df["RainToday"] == 'No', ["Location", "Year"]]
 
-lluviaYes = df[df['RainToday'] == 'Yes' ]
-lluviaNO = df[df['RainToday'] == 'No']
-lNO = lluviaNO.groupby(['Year', 'RainToday'])['RainToday'].count()
-lou = lluviaYes['RainToday']
+#lluviaYes = df[df['RainToday'] == 'Yes' ]
+#lluviaNO = df[df['RainToday'] == 'No']
+#lNO = lluviaNO.groupby(['Year', 'RainToday'])['RainToday'].count()
+#lou = lluviaYes['RainToday']
 #print(lou)
 
 
 #cantidad de lluvia por año según ciudad
 
-df3 = lluviaYes.groupby(['Year','Location'])['RainToday'].count().reset_index(name='LLuviaCOUNT')
+#df3 = lluviaYes.groupby(['Year','Location'])['RainToday'].count().reset_index(name='LLuviaCOUNT')
 #print(df3)
 
 #array ciudades
@@ -51,7 +53,6 @@ for i in ciudadess.index:
     count = count + 1
     citys.append(str(i))
     indices.append(count)
-
 
 localidades = { 'indice': indices, 'citys' : citys}
 df02 = pd.DataFrame(data = localidades)
@@ -67,22 +68,53 @@ if busqueda in cdsID:
     found = str(cds[busqueda-1])
     local = df[df['Location'] == found]
     print ('Ciudad: '+found)
-    df4 = local.groupby(['Year', 'RainToday'])['RainToday'].count().reset_index(name='Cantidad')#.sort_values(by='Cantidad', ascending=False)
-    print(df4)
+    df4 = local.groupby(['Year', 'RainToday'])['RainToday'].count().reset_index(name='Cantidad') #.sort_values(by='Cantidad', ascending=False)
+
+    #separo los datos logisticamente
+    df4yes = df4[df4['RainToday'] == 'Yes']
+    df4no = df4[df4['RainToday'] == 'No']
+
+    # PROMEDIO SEGÚN SI LLUVIO O NO
+    var = 0
+    cont = 0
+    for i in df4yes.index:
+        cont = cont + 1
+        var = var + df4yes['Cantidad'][i]
+        prom = var / cont
+    varno = 0
+    contno = 0
+    for n in df4no.index:
+        contno = contno + 1
+        varno = varno + df4no['Cantidad'][n]
+        promno = varno / contno
+    print(str(df4)+'\nEl promedio total de lluvia es:\t'+str(prom)+'\nEl promedio total de sin lluvia es:\t'+str(promno))
+
+
+
+
+    #elimino las columnas innecesarias para mi analisis
+    #df4yes.drop(['RainToday'], axis = 1, inplace=True)
+    #df4no.drop(['RainToday'], axis = 1, inplace=True)
+    
+    #yrs = np.array(df4yes['Year']).reshape((1, -1))
+    #cntidad = np.array(df4yes['Cantidad'])
+
+    #X = yrs
+    #X = X.transpose()
+    #y = cntidad
+    #print(X.shape)
+    #print(y.shape)
+    #plt.scatter(X,y)
+    #plt.title('Cantidad de veces que ha llovido por año en: '+found)
+    #plt.xlabel('Años')
+    #plt.ylabel('Cantidad ')
+    #plt.show()
+
+
 else:
     print("Digitalice el id correspondiente, estos pueden ser\n"+str(cxd))
     print (""" \t TIPO
 ID\t Ciudad""")
-        
-
-#for i in range(cds)
-
-# ciudad adelaide
-local = df[df['Location'] == 'Adelaide']
-
-# cantidad de veces y las que no ha llovido en adelaide según groupby de años y lluvia (yes or no)
-#df4 = local.groupby(['Year', 'RainToday'])['RainToday'].count().reset_index(name='Cantidad')#.sort_values(by='Cantidad', ascending=False)
-
 
 #print('Datos 2017 si ha llovido o no en adelaide')
 
@@ -106,6 +138,17 @@ local = df[df['Location'] == 'Adelaide']
 
 #print('Cantidad de veces que no lluvio en Adelaide: '+ str(anoNO))
 #print('Cantidad de veces que lluvio en Adelaide: '+ str(anoYES))
+
+#for i in range(cds)
+
+# ciudad adelaide
+local = df[df['Location'] == 'Adelaide']
+
+# cantidad de veces y las que no ha llovido en adelaide según groupby de años y lluvia (yes or no)
+#df4 = local.groupby(['Year', 'RainToday'])['RainToday'].count().reset_index(name='Cantidad')#.sort_values(by='Cantidad', ascending=False)
+
+
+
 
 
 #print('Datos anuales si ha llovido o no en adelaide')
